886e5e1ea4c99e4cd7d8aca5c157bb66ef2ae42a
santimarce <marchulo1996@gmail.com>
2022-12-08 21:34:43 -0500

    trabajoclase1

--- ejer3.py
+++ ejer3.py
@@ -8,6 +8,12 @@
 #***
 #**
 #*
-
-
-        
+def patron(c):    
+    for i in range(1,6):
+        c=c+'*'
+        print(c)
+    for j in range(1,5):
+        c = c[:-1]
+        print(c)
+         
+dibujo = patron('00000')
